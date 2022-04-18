51fb36e8af1f8b77272c748332d9a0c7cd2a2aa2
Clifford Nelson <cliffordryannelson@gmail.com>
2022-04-18 13:14:53 -0700

    add string support

--- code.py
+++ code.py
@@ -2,6 +2,10 @@
 # SPDX-License-Identifier: MIT
 # RaspberryPi Pico RP2040 Mechanical Keyboard
 
+# https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/consumer_control_code.html
+# https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/mouse.html
+# https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/keycode.html
+
 import time
 import board
 from digitalio import DigitalInOut, Direction, Pull
@@ -11,6 +15,9 @@ from adafruit_hid.keycode import Keycode
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+
 print("---Pico Pad Keyboard---")
 
 led = DigitalInOut(board.LED)
@@ -19,6 +26,7 @@ led.value = True
 
 kbd = Keyboard(usb_hid.devices)
 cc = ConsumerControl(usb_hid.devices)
+layout = KeyboardLayoutUS(kbd)
 
 # list of pins to use (skipping GP15 on Pico because it's funky)
 pins = (
@@ -36,22 +44,32 @@ pins = (
 
 MEDIA = 1
 KEY = 2
+TYPE = 3
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ BELOW -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
 # -_-_-_-_-_-_-_-_-_-_ CHANGE THE KEYBOARD BUTTONS -_-_-_-_-_-_-_-_-_-_ #
+# https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/consumer_control_code.html
+# https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/mouse.html
+# https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/keycode.html
+# (MEDIA, ConsumerControlCode.VOLUME_DECREMENT),
+# (MEDIA, ConsumerControlCode.VOLUME_INCREMENT),
+# (TYPE, ("Hello World!\n")),
+# (KEY, [Keycode.ONE]),
+# (KEY, (Keycode.GUI, Keycode.C)),
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ BELOW -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+
 keymap = {
-    (0): (KEY, (Keycode.GUI, Keycode.C)),
-    (1): (KEY, (Keycode.GUI, Keycode.V)),
+    (0): (KEY, [Keycode.ONE]),
+    (1): (KEY, [Keycode.TWO]),
     (2): (KEY, [Keycode.THREE]),
     (3): (KEY, [Keycode.FOUR]),
     (4): (KEY, [Keycode.FIVE]),
 
-    (5): (MEDIA, ConsumerControlCode.VOLUME_DECREMENT),
-    (6): (MEDIA, ConsumerControlCode.VOLUME_INCREMENT),
-    (7): (KEY, [Keycode.R]),
-    (8): (KEY, [Keycode.G]),
-    (9): (KEY, [Keycode.B])
+    (5): (TYPE, ("Hello World!\n")),
+    (6): (KEY, (Keycode.SHIFT, Keycode.LEFT_ARROW)),
+    (7): (KEY, (Keycode.GUI, Keycode.C)),
+    (8): (KEY, (Keycode.GUI, Keycode.V)),
+    (9): (KEY, [Keycode.LEFT_SHIFT])
 }
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ ABOVE -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
 # -_-_-_-_-_-_-_-_-_-_ CHANGE THE KEYBOARD BUTTONS -_-_-_-_-_-_-_-_-_-_ #
@@ -74,6 +92,8 @@ while True:
                 try:
                     if keymap[button][0] == KEY:
                         kbd.press(*keymap[button][1])
+                    elif keymap[button][0] == TYPE:
+                        layout.write(keymap[button][1])
                     else:
                         cc.send(keymap[button][1])
                 except ValueError:  # deals w six key limit
@@ -91,3 +111,4 @@ while True:
                 switch_state[button] = 0
 
     time.sleep(0.01)  # debouncer
+
